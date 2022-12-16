3ef1032d303ea4272fe6b5f6fd1ebfa5e2dcfe29
owentashlin <owentashlin@gmail.com>
2022-12-16 13:40:54 -0800

    working on pdf viewer

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+function NavBar() {
+
+    return (
+    <div className='nav'>
+    <ul className='nav-list'>
+      <li className='nav-button'>
+      <Link className='a' to='/'>Home</Link>
+      </li>
+      <li>
+      <Link className='a' to='/about'>About</Link>
+      </li>
+      <li>
+      <Link className='a' to='/portfolio'>Portfolio</Link>
+      </li>
+      <li>
+      <Link className='a' to='/contact'>Contact</Link>
+      </li>
+    </ul>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+    return ( 
+        <p>lets talk about me</p>
+     );
+}
+
+export default About;
