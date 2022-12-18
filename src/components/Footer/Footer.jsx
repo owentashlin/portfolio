e41c090649eaf9d90d62308d889bdd214a502125
owentashlin <owentashlin@gmail.com>
2022-12-18 13:46:41 -0800

    footer working

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
+
+import {FaGithubSquare} from "react-icons/fa";
+import {FaLinkedin} from 'react-icons/fa'
+import {ImMail} from 'react-icons/im'
+import './Footer.css'
+
 function Footer() {
+    function Mailto({ email, subject, body, ...props }) {
+        return (
+          <a href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>
+            {props.children}
+          </a>
+        );
+      }
+
     return ( 
-        <div>
-            
+        <div className='footer'>
+            <a className='footer-link' href='https://github.com/owentashlin'><FaGithubSquare/></a>
+            <Mailto email="owentashlin@gmail.com" subject="Hello" body="Hello world!">
+            <ImMail/></Mailto>
+            <a className='footer-link' href='https://www.linkedin.com/in/owen-tashlin/'><FaLinkedin/></a>
         </div>
      );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,11 @@ import './Home.css'
 function Home() {
     return (
         <>
-        <img src='/' alt='headshot'/>
-        <p>image goes here when i can figure out why the fucking image won't load</p>
         <p>i am a homepage</p>
         </> 
      )
 }
+
+
 
 export default Home;
