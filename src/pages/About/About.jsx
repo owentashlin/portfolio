7867264586eb54604a1e1a94cfd71113f70cd82b
owentashlin <owentashlin@gmail.com>
2022-12-22 19:11:36 -0800

    links and style update on portfolio page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,12 @@
+import './About.css'
+
 function About() {
     return ( 
-        <>
-            <p>lets talk about me</p>
-            <img src="https://i.imgur.com/7o0HM6x.jpg" title="source: imgur.com" className='headshot'/>
-        </>
+        <div className='about'>
+            <h1>lets talk about me</h1>
+            <p>Coming out of a design and invention background, I approach front end and full stack software development as an opportunity to find new solutions. I excel in environments that allow for collaboration and free exchange of ideas, as many eyes on a problem will always yield a better resolution in the end. I have extensive customer service experience in both media and medical environments, which has given me a good understanding of how a user experience effects the impression of a brand as a whole. This combination of problem solving mentality and understanding of customer experience has given me a valuable perspective when training and supervising others, as well as in my own work.</p>
+            <img src="https://i.imgur.com/7o0HM6x.jpg" title="source: imgur.com" className='headshot' alt='headshot' />
+        </div>
      );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,9 @@ import './Home.css'
 function Home() {
     return (
         <>
-        <p>i am a homepage</p>
+        <p>hello! my name is owen tashlin.</p>
+        <p>i approach software engineering and design like any other design process, starting with a need and breaking it down to see where it goes. My designs principles lean towards <em>simple but effective</em> and prioritize ease of use and discoverability to enhance the user experience.</p>
+        <p>checkout out my portfolio for some examples of projects, or find me on linkedin and github.</p>
         </> 
      )
 }
