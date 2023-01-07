f35bf94a8367fda41412bdeb66dafc201854fd8c
owentashlin <owentashlin@gmail.com>
2023-01-06 17:57:14 -0800

    resume viewer fix

--- src/components/Resume/Resume-Viewer.jsx
+++ src/components/Resume/Resume-Viewer.jsx
@@ -1,37 +1,8 @@
-import React, {useState} from 'react'
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
-import './Resume-Viewer.css'
-
-
 function ResumeViewer() {
-    const [numPages, setNumPages] = useState(null)
-	const [pageNumber, setPageNumber] = useState(1)
-
-	const onDocumentLoadSuccess = ({numPages}) => {
-		setNumPages(numPages)
-	}
-
-	const goToPrevPage = () =>
-		setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1)
-
-	const goToNextPage = () =>
-		setPageNumber(
-			pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
-		)
-
 	return (
 		<div>
-			<div>
-				<button onClick={goToPrevPage}>Prev</button>
-				|
-				<button onClick={goToNextPage}>Next</button>
-				<p>
-					Page {pageNumber} of {numPages}
-				</p>
-			</div>
-			<Document file="Owen_Tashlin_Resume.pdf" onLoadSuccess ={onDocumentLoadSuccess}>
-				<Page pageNumber={pageNumber} />
-			</Document>
+			<img src="https://i.imgur.com/jT7vYPR.jpg" title="source: imgur.com" alt='resume page 1'/>
+			<img src="https://i.imgur.com/8zv4SAq.jpg" title="source: imgur.com" alt='resume page 2'/>
 		</div>
 	);
 }
