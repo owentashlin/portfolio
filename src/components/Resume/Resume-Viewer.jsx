0969b4dd5d9f6cd4dedc0d1b258989f73143a9b2
owentashlin <owentashlin@gmail.com>
2023-01-24 10:03:45 -0500

    resume update

--- src/components/Resume/Resume-Viewer.jsx
+++ src/components/Resume/Resume-Viewer.jsx
@@ -1,7 +1,7 @@
 function ResumeViewer() {
 	return (
 		<div>
-			<img src="https://i.imgur.com/ovn4Sr7.jpg" title="source: imgur.com" alt='resume page 1'/>
+			<img src="https://i.imgur.com/hqBU6LG.jpg" title="source: imgur.com" alt='resume page 1'/>
 			<img src="https://i.imgur.com/8zv4SAq.jpg" title="source: imgur.com" alt='resume page 2'/>
 		</div>
 	);
