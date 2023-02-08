71e66c1e5e6b55133313b414488f080c729efb5c
owentashlin <owentashlin@gmail.com>
2023-02-07 16:22:18 -0800

    fixed image link

--- src/components/Resume/Resume-Viewer.jsx
+++ src/components/Resume/Resume-Viewer.jsx
@@ -2,11 +2,13 @@ function ResumeViewer() {
 	return (
 		<div>
 			<img src="https://i.imgur.com/hqBU6LG.jpg" title="source: imgur.com" alt='resume page 1'/>
-			<img src="https://i.imgur.com/8zv4SAq.jpg" title="source: imgur.com" alt='resume page 2'/>
+			<img src="https://i.imgur.com/mXW1fSC.jpg" title="source: imgur.com" alt='resume page 2'/>
 		</div>
 	);
 }
 
 export default ResumeViewer;
+
+
 
 
