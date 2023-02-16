9971c6e4125fb3efe9cca8eb1cdae99e6ce3eec8
owentashlin <owentashlin@gmail.com>
2023-02-16 13:21:22 -0800

    fix resume image size

--- src/components/Resume/Resume-Viewer.jsx
+++ src/components/Resume/Resume-Viewer.jsx
@@ -1,8 +1,8 @@
 function ResumeViewer() {
 	return (
 		<div>
-			<img src="https://i.imgur.com/RpmNXmZ.png" title="source: imgur.com" alt='resume page 1'/>
-			<img src="https://i.imgur.com/kLrHOaH.png" title="source: imgur.com" alt='resume page 2'/>
+			<img src="https://i.imgur.com/zmV6O97.png" title="source: imgur.com" alt='resume page 1'/>
+			<img src="https://i.imgur.com/aJsnQNq.png" title="source: imgur.com" alt='resume page 2'/>
 		</div>
 	);
 }
