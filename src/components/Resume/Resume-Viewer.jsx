71ea99625513b29a83d23d43e4bd14aac40d915b
owentashlin <owentashlin@gmail.com>
2023-02-16 13:18:24 -0800

    update resume download pdf and images

--- src/components/Resume/Resume-Viewer.jsx
+++ src/components/Resume/Resume-Viewer.jsx
@@ -1,8 +1,8 @@
 function ResumeViewer() {
 	return (
 		<div>
-			<img src="https://i.imgur.com/hqBU6LG.jpg" title="source: imgur.com" alt='resume page 1'/>
-			<img src="https://i.imgur.com/mXW1fSC.jpg" title="source: imgur.com" alt='resume page 2'/>
+			<img src="https://i.imgur.com/RpmNXmZ.png" title="source: imgur.com" alt='resume page 1'/>
+			<img src="https://i.imgur.com/kLrHOaH.png" title="source: imgur.com" alt='resume page 2'/>
 		</div>
 	);
 }
