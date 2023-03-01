00db6ba0c179bd99c9d97d732a233c505b710d55
owentashlin <owentashlin@gmail.com>
2023-03-01 14:32:33 -0800

    style update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import {GiAnvilImpact} from 'react-icons/gi'
+import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
 
 import './NavBar.css'
 
@@ -8,7 +10,7 @@ function NavBar() {
     <>
       <nav> 
         <div className='nav-list'>
-          <a className='nav-link' id='simon' href='https://owentashlin.github.io/GA--project-1-Simon/'><img src='new_logo_small.png' alt='logo'/></a>
+          <div><a className='nav-link' id='simon' href='https://owentashlin.github.io/GA--project-1-Simon/'><IoIosArrowBack size='30px'/><GiAnvilImpact size='35px'/><IoIosArrowForward size='30px'/></a></div>
           <Link className='nav-link' to='/'>Home</Link>
           <Link className='nav-link' to='/about'>About</Link>
           <Link className='nav-link' to='/portfolio'>Portfolio</Link>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
-import './Home.css'
+import "./Home.css"
+
 function Home() {
+let linkedinUrl = 'https://www.linkedin.com/in/owen-tashlin/'
+
     return (
         <div className='home'>
         <h1 className='home-header'>Hi! My name is Owen Tashlin.</h1>
@@ -7,7 +10,7 @@ function Home() {
             
         <p className='home-text'>I start with a need and break it down to see where it goes. My design principles lean toward <strong>simple but effective</strong> solutions, prioritize <strong>ease of use</strong>, and <strong>discoverability</strong> to enhance the user experience.</p>
 
-        <p className='home-text'>Checkout out my Portfolio for some examples of recent projects, or find me on <strong>LinkedIn and GitHub</strong>.</p>
+        <p className='home-text'>Checkout out my Portfolio for some examples of recent projects, or find me on  <a className='home-link' href={linkedinUrl}>LinkedIn</a>.</p>
 
         <img src="/headshot-old.jpg" className='headshot' alt='headshot'/>
         </div> 
