dadb0963272379b220fb81a34980166be42148ce
owentashlin <owentashlin@gmail.com>
2023-03-03 14:24:00 -0800

    updated links

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -34,7 +34,7 @@ function About() {
             <br></br>
 
             <div className='about-logo'>
-            <a href='https://jovial-elf-4f61ec.netlify.app/'><img src='/simon-big.svg' alt='logo' id='rotate-center'/></a>
+            <a href='https://owensworkshop.netlify.app/'><img src='/simon-big.svg' alt='logo' id='rotate-center'/></a>
             </div>
             <br></br>
             <br></br>
