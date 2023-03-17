063e07efc20d893e578badc8e793f4717727088e
owentashlin <owentashlin@gmail.com>
2023-03-16 21:12:19 -0700

    text color change on about page for clarity

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,13 +6,13 @@ function About() {
             <img src="/headshot-old.jpg" className='headshot' alt='headshot'/>
             <h1 className='about-header' >Full Stack Software Engineer, Product Designer, Fiber Artist</h1>
 
-            <p>Coming out of a <strong>design and invention </strong>background, I approach front end and full stack software development as an opportunity to find new <strong>solutions</strong>.</p>
+            <p className='about-p'>Coming out of a <strong>design and invention </strong>background, I approach front end and full stack software development as an opportunity to find new <strong>solutions</strong>.</p>
 
-            <p>While environments that allow for <strong>collaboration</strong> and free exchange of ideas are my preference (the more eyes on a problem, the better!), I am also comfortable working solo. </p>
+            <p className='about-p'>While environments that allow for <strong>collaboration</strong> and free exchange of ideas are my preference (the more eyes on a problem, the better!), I am also comfortable working solo. </p>
 
-            <p>Over a decade working with the public in a variety of fields has given me a good understanding of how a <strong>user experience</strong> can affect not only feelings about a product, but about the impression of a brand as a whole.</p> 
+            <p className='about-p'>Over a decade working with the public in a variety of fields has given me a good understanding of how a <strong>user experience</strong> can affect not only feelings about a product, but about the impression of a brand as a whole.</p> 
 
-            <p>This combination of <strong>problem solving</strong> mentality and understanding of customer experience has given me a valuable perspective when <strong>training and supervising</strong> others, as well as in my own work.</p>
+            <p className='about-p'>This combination of <strong>problem solving</strong> mentality and understanding of customer experience has given me a valuable perspective when <strong>training and supervising</strong> others, as well as in my own work.</p>
 
             <div className='image-list'>
             <p>These are my current preferred languages and frameworks, but I am always looking to learn more!</p>
